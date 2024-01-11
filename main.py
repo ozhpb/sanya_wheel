a496d9e5308e6d60b167bb96643569bc2263ff04
Ozhigov Pavel <ozhigov.pavel10@gmail.com>
2024-01-12 00:29:18 +0800

    Добавление скрипта с бесконечным циклом и возвратом ответа показателей ОЗУ

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import os
+from datetime import datetime
+
+
+def main():
+    datetime_now = datetime.now()
+    memory_response = os.popen("free").read()
+    print(f"\nПоказатели памяти на {datetime_now} такие.\nТвоё дело распарсить ответ и сохранить показатели в файл | БД | др.\n")
+    print(memory_response)
+
+
+if __name__ == "__main__":
+    print("Для остановки скрипта нажми Ctrl + C")
+    while True:
+        try:
+            time.sleep(1)
+            main()
+        except KeyboardInterrupt:
+            print("\nТы завершил работу скрпта. Ай ай ай.")
+            break
