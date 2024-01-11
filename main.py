c59c75ec0ebdb8ad70bfffb9f898c7e93d076e8b
Ozhigov Pavel <ozhigov.pavel10@gmail.com>
2024-01-12 01:04:26 +0800

    Разбиение по функциям, добавление функции получения текущего хоста.

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 import time
 import os
+import sys
 from datetime import datetime
 
 
-def main():
+def get_datetime_now():
+    """
+    Вернуть текущие дату и время.
+    TODO: вернуть в нужном формате.
+    """
     datetime_now = datetime.now()
+    return datetime_now
+
+
+def get_memory():
+    """
+    Вернуть ответ команды free.
+    TODO: распарсить ответ.
+    """
     memory_response = os.popen("free").read()
-    print(f"\nПоказатели памяти на {datetime_now} такие."
-          "\nТвоё дело распарсить ответ и сохранить"
-          " показатели в файл | БД | др.\n")
-    print(memory_response)
+    return memory_response
+
+
+def get_host():
+    """
+    Вернуть hostname тачки.
+    """
+    host = os.uname()[1]
+    return host
+
+
+def get_output_response(host, datetime_now, memory_response):
+    """
+    Вернуть ответ скрипта. Куда писать ответ, напишешь сам далее.
+    """
+    response = str(datetime_now) + "\n"
+    response += f"Показатели памяти {host}:\n"
+    response += memory_response + "\n"
+    response += "Твоё дело распарсить ответ и сохранить"
+    response += " показатели в файл | БД | др.\n"
+    return response
+
+
+def main():
+    """
+    Вызов методов и вывод ответа программы.
+    """
+    datetime_now = get_datetime_now()
+    memory = get_memory()
+    host = get_host()
+    output_response = get_output_response(host, datetime_now, memory)
+    print(output_response)
 
 
 if __name__ == "__main__":
